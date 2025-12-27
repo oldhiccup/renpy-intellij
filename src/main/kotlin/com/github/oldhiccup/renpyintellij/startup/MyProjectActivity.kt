25e8225d121ec5a1604e4b626ce36395971c43f3
GitHub Action <action@github.com>
2025-12-27 12:57:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/oldhiccup/renpyintellij/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/oldhiccup/renpyintellij/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.oldhiccup.renpyintellij.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
